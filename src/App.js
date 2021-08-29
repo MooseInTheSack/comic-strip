62fcfedad5de3936cae60c6eee8bac0b45a19551
Tyler Kirkpatrick <tyl3rkirkpatrick@yahoo.com>
2021-08-29 12:31:45 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { Fragment }  from 'react';
 import './App.css';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Container from '@material-ui/core/Container';
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import Carousel from './components/Carousel/Carousel'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+      <div className="App">
+      <Container maxWidth="md">
+
+        <Route
+          path="/"
+          render={({ location }) => (
+            <Fragment>
+              <Tabs centered={true} value={location.pathname}>
+                <Tab value="/" label="Home" component={Link} to="/" />
+                <Tab value="/carousel" label="Carousel" component={Link} to="/carousel" />
+                
+              </Tabs>
+              <Switch>
+                <Route path="/carousel">
+                  <Carousel />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </Fragment>
+          )}
+        />
+        </Container>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/imagePathsObject.js
@@ -0,0 +1,397 @@
+import avengersPoster from './components/common/images/Avengers_poster.jpg'
+import ben3 from './components/common/images/ben_3.jpg'
+import ben4 from './components/common/images/Ben_4.jpg'
+import blackmane from './components/common/images/blackmane.jpg'
+import BuddyTheAnt from './components/common/images/Buddy_the_ant.jpg'
+import Dr_Ferald from './components/common/images/Dr_Ferald.jpg'
+import fisherman from './components/common/images/fisherman.jpg'
+import Ho_ho_whore from './components/common/images/Ho_ho_whore.jpg'
+import iron_man_vs_thanos from './components/common/images/iron_man_vs_thanos.jpg'
+import new_buddy from './components/common/images/new_buddy.jpg'
+import rodrigo_2099 from './components/common/images/rodrigo_2099.jpg'
+import Rumple_and_Horace from './components/common/images/Rumple_and_Horace.jpg'
+import scarecrow_boy from './components/common/images/scarecrow_boy.jpg'
+import self_p from './components/common/images/self_p.jpg'
+import self_portrait_copy from './components/common/images/self_portrait_copy.jpg'
+import sourcerer from './components/common/images/sourcerer.jpg'
+import spiderverse from './components/common/images/spiderverse.jpg'
+import treasure_hunters from './components/common/images/treasure_hunters.png'
+import Way_out_West_image from './components/common/images/Way_out_West_image.jpg'
+import i001 from './components/common/images/001.jpg'
+import i002 from './components/common/images/002.jpg'
+import i003 from './components/common/images/003.jpg'
+import i004 from './components/common/images/004.jpg'
+import i005 from './components/common/images/005.jpg'
+import i006 from './components/common/images/006.jpg'
+import i007 from './components/common/images/007.jpg'
+import scorpion from './components/common/images/scorpion.jpg'
+import i009 from './components/common/images/009.jpg'
+import i010 from './components/common/images/010.jpg'
+import i011 from './components/common/images/011.jpg'
+import i012 from './components/common/images/012.jpg'
+import i013 from './components/common/images/013.jpg'
+import i014 from './components/common/images/014.jpg'
+import i015 from './components/common/images/015.jpg'
+import i016 from './components/common/images/016.jpg'
+import i017 from './components/common/images/017.jpg'
+import buddy_3 from './components/common/images/buddy_3.jpg'
+import buddy_2 from './components/common/images/buddy_2.jpg'
+
+import IMG_1762 from './components/common/images/IMG_1762.jpeg'
+import IMG_1775 from './components/common/images/IMG_1775.jpeg'
+import IMG_1804 from './components/common/images/IMG_1804.jpeg'
+import IMG_1808 from './components/common/images/IMG_1808.jpeg'
+import IMG_1552 from './components/common/images/IMG_1552.jpeg'
+import IMG_1575 from './components/common/images/IMG_1575.jpeg'
+import IMG_1768 from './components/common/images/IMG_1768.jpeg'
+import IMG_1786 from './components/common/images/IMG_1786.jpeg'
+import IMG_1807 from './components/common/images/IMG_1807.jpeg'
+import IMG_1809 from './components/common/images/IMG_1809.jpeg'
+
+export const imagesObjects = [
+        { 
+            original: avengersPoster,
+            thumbnail: avengersPoster,
+            title: 'avengersPoster',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: ben3,
+            thumbnail: ben3,
+            title: 'ben3',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: ben4,
+            thumbnail: ben4,
+            title: 'ben4',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: blackmane,
+            thumbnail: blackmane,
+            title: 'blackmane',
+            rows: 3,
+            cols: 2,
+        },
+        { 
+            original: Dr_Ferald,
+            thumbnail: Dr_Ferald,
+            title: 'Dr_Ferald',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: fisherman,
+            thumbnail: fisherman,
+            title: 'fisherman',
+            rows: 3,
+            cols: 2,
+        },
+        { 
+            original: BuddyTheAnt,
+            thumbnail: BuddyTheAnt,
+            title: 'BuddyTheAnt',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: Ho_ho_whore,
+            thumbnail: Ho_ho_whore,
+            title: 'Ho_ho_whore',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: iron_man_vs_thanos,
+            thumbnail: iron_man_vs_thanos,
+            title: 'iron_man_vs_thanos',
+            rows: 3,
+            cols: 2,
+        },
+        { 
+            original: new_buddy,
+            thumbnail: new_buddy,
+            title: 'new_buddy',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: rodrigo_2099,
+            thumbnail: rodrigo_2099,
+            title: 'rodrigo_2099',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: scarecrow_boy,
+            thumbnail: scarecrow_boy,
+            title: 'scarecrow_boy',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: Rumple_and_Horace,
+            thumbnail: Rumple_and_Horace,
+            title: 'Rumple_and_Horace',
+            rows: 3,
+            cols: 2,
+        },
+        /*
+        { 
+            original: treasure_hunters,
+            thumbnail: treasure_hunters,
+            title: 'treasure_hunters',
+            rows: 3,
+            cols: 1,
+        },
+        */
+        { 
+            original: spiderverse,
+            thumbnail: spiderverse,
+            title: 'spiderverse',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: sourcerer,
+            thumbnail: sourcerer,
+            title: 'sourcerer',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: self_portrait_copy,
+            thumbnail: self_portrait_copy,
+            title: 'self_portrait_copy',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: self_p,
+            thumbnail: self_p,
+            title: 'self_p',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: Way_out_West_image,
+            thumbnail: Way_out_West_image,
+            title: 'Way_out_West_image',
+            rows: 3,
+            cols: 2,
+        },
+        { 
+            original: i001,
+            thumbnail: i001,
+            title: 'i001',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: scorpion,
+            thumbnail: scorpion,
+            title: 'scorpion',
+            rows: 4,
+            cols: 2,
+        },
+        { 
+            original: treasure_hunters,
+            thumbnail: treasure_hunters,
+            title: 'treasure_hunters',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i002,
+            thumbnail: i002,
+            title: 'i002',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i003,
+            thumbnail: i003,
+            title: 'i003',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i004,
+            thumbnail: i004,
+            title: 'i004',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i005,
+            thumbnail: i005,
+            title: 'i005',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i006,
+            thumbnail: i006,
+            title: 'i006',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i007,
+            thumbnail: i007,
+            title: 'i007',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i009,
+            thumbnail: i009,
+            title: 'i009',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i010,
+            thumbnail: i010,
+            title: 'i010',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i011,
+            thumbnail: i011,
+            title: 'i011',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i012,
+            thumbnail: i012,
+            title: 'i012',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i013,
+            thumbnail: i013,
+            title: 'i013',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i014,
+            thumbnail: i014,
+            title: 'i014',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i015,
+            thumbnail: i015,
+            title: 'i015',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i016,
+            thumbnail: i016,
+            title: 'i016',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: i017,
+            thumbnail: i017,
+            title: 'i017',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: buddy_3,
+            thumbnail: buddy_3,
+            title: 'buddy_3',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1762,
+            thumbnail: IMG_1762,
+            title: 'Better Call Saul!',
+            rows: 3,
+            cols: 2,
+        },
+        { 
+            original: buddy_2,
+            thumbnail: buddy_2,
+            title: 'buddy_2',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1775,
+            thumbnail: IMG_1775,
+            title: 'IMG_1775',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1804,
+            thumbnail: IMG_1804,
+            title: 'IMG_1804',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1808,
+            thumbnail: IMG_1808,
+            title: 'IMG_1808',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1552,
+            thumbnail: IMG_1552,
+            title: 'Master Chief',
+            rows: 3,
+            cols: 2,
+        },
+        { 
+            original: IMG_1575,
+            thumbnail: IMG_1575,
+            title: 'IMG_1575',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1768,
+            thumbnail: IMG_1768,
+            title: 'IMG_1768',
+            rows: 3,
+            cols: 2,
+        },
+        { 
+            original: IMG_1786,
+            thumbnail: IMG_1786,
+            title: 'IMG_1786',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1807,
+            thumbnail: IMG_1807,
+            title: 'IMG_1807',
+            rows: 3,
+            cols: 1,
+        },
+        { 
+            original: IMG_1809,
+            thumbnail: IMG_1809,
+            title: 'IMG_1809',
+            rows: 3,
+            cols: 1,
+    },].
+    reverse()
