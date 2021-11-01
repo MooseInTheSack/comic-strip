b2e070fc18f3c29e00046b5864499ebe0acce26d
Tyler Kirkpatrick <tyl3rkirkpatrick@yahoo.com>
2021-11-01 15:00:06 -0500

    added small update to About just to see if github actions will finally start working

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -29,7 +29,7 @@ export default function Carousel() {
         <p>We are three brothers who have too much free time on our hands so we decided to make a web comic series.</p>
         <p>Both Tyler and Ryan come up with the ideas, writing, and rough sketches.</p>
         <p>Ryan, the only artistically talented of the bunch, creates the finished comic. Sometimes Tyler makes them but the actual good ones are Ryan's.</p>
-        <p>And finally, Nathan posts the masterpiece across different mediums like Instagram and Facebook.</p>
+        <p>And finally, Nathan posts the masterpiece across different mediums like Instagram, Facebook, and Reddit.</p>
     </div>
   );
 }
