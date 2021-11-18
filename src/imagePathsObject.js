640dbe33854ddfd0a66e4fcfb20887e0de9f088d
MooseInTheSack <tyl3rkirkpatrick@yahoo.com>
2021-11-18 13:41:18 -0600

    added 3 new comics, changed column property to match row prop for each image

--- src/imagePathsObject.js
+++ src/imagePathsObject.js
@@ -12,82 +12,107 @@ import grapes from './components/common/images/grapes_perfect.png'
 import writersBlock from './components/common/images/writers_block.png'
 import desserts_from_dis from './components/common/images/desserts_from_dis.png'
 
+import goblin from './components/common/images/goblin.jpg'
+import meth from './components/common/images/2_meth.png'
+import pools_closed from './components/common/images/pools_closed.png'
+
 export const imagesObjects = [
         { 
             original: first_comic,
             thumbnail: first_comic,
             title: 'Experience',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: banana_comic,
             thumbnail: banana_comic,
             title: 'Banana',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: camping,
             thumbnail: camping,
             title: 'The Stars',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: pirates,
             thumbnail: pirates,
             title: 'Yarrrr',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: venmo,
             thumbnail: venmo,
             title: 'Venmo > CashApp > PayPal > V-Bucks',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: honesty,
             thumbnail: honesty,
             title: 'Honesty is the best (HR) Policy',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: smile,
             thumbnail: smile,
             title: 'Let\'s put a smile on that face!',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: carrot,
             thumbnail: carrot,
             title: 'Muzzle',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: grapes,
             thumbnail: grapes,
             title: 'Grapes of Wrath',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: desserts_from_dis,
             thumbnail: desserts_from_dis,
             title: 'Grandma\'s Cookies',
             rows: 3,
-            cols: 1,
+            cols: 3,
         },
         { 
             original: writersBlock,
             thumbnail: writersBlock,
             title: 'Writers Block',
             rows: 3,
-            cols: 1,
+            cols: 3,
+        },
+        { 
+            original: goblin,
+            thumbnail: goblin,
+            title: 'Goblin',
+            rows: 3,
+            cols: 3,
+        },
+        { 
+            original: meth,
+            thumbnail: meth,
+            title: 'Writers Block',
+            rows: 3,
+            cols: 3,
+        },
+        { 
+            original: pools_closed,
+            thumbnail: pools_closed,
+            title: 'Pool\'s Closed',
+            rows: 3,
+            cols: 3,
         },
     ].reverse()
